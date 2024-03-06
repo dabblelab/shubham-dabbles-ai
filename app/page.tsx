bb7f3e2f81037545df3a7035a0c6f6217cbe0315
Shubham Prakash <28767301+iShubhamPrakash@users.noreply.github.com>
2024-03-07 02:37:38 +0530

    Basic chat working

--- app/page.tsx
+++ app/page.tsx
@@ -3,66 +3,41 @@ import { ChatWindow } from "@/components/ChatWindow";
 export default function Home() {
   const InfoCard = (
     <div className="p-4 md:p-8 rounded bg-[#25252d] w-full max-h-[85%] overflow-hidden">
-      <h1 className="text-3xl md:text-4xl mb-4">
-        ▲ Next.js + LangChain.js 🦜🔗
-      </h1>
+      <div className="text-center max-w-xl mx-auto">
+        <h1 className="text-3xl md:text-4xl mb-4">Archr Assistant</h1>
+        <h2 className="text-xl mb-6">
+          Transform Ideas into Plans with Archr 🚀
+        </h2>
+      </div>
+      <p>
+        Your AI-Powered Ally in Transforming App Ideas into Detailed Project
+        Roadmaps. Archr is crafted to seamlessly navigate the journey from the
+        initial flicker of creativity to a structured software development
+        pathway.
+      </p>
       <ul>
         <li className="text-l">
           🤝
           <span className="ml-2">
-            This template showcases a simple chatbot using{" "}
-            <a href="https://js.langchain.com/" target="_blank">
-              LangChain.js
-            </a>{" "}
-            and the Vercel{" "}
-            <a href="https://sdk.vercel.ai/docs" target="_blank">
-              AI SDK
-            </a>{" "}
-            in a{" "}
-            <a href="https://nextjs.org/" target="_blank">
-              Next.js
-            </a>{" "}
-            project.
-          </span>
-        </li>
-        <li className="hidden text-l md:block">
-          💻
-          <span className="ml-2">
-            You can find the prompt and model logic for this use-case in{" "}
-            <code>app/api/chat/route.ts</code>.
+            <strong>Simple Start:</strong> Just bring your app idea, no tech
+            talk needed.
           </span>
         </li>
         <li>
-          🏴‍☠️
-          <span className="ml-2">
-            By default, the bot is pretending to be a pirate, but you can change
-            the prompt to whatever you want!
+          📈
+          <span>
+            <strong> Detailed Planning:</strong>{" "}
+            {`Get a complete blueprint,
+            including what your app will do and how it'll work, without needing
+            any software expertise.`}
           </span>
         </li>
-        <li className="hidden text-l md:block">
-          🎨
+        <li>
+          💵
           <span className="ml-2">
-            The main frontend logic is found in <code>app/page.tsx</code>.
-          </span>
-        </li>
-        <li className="text-l">
-          🐙
-          <span className="ml-2">
-            This template is open source - you can see the source code and
-            deploy your own version{" "}
-            <a
-              href="https://github.com/langchain-ai/langchain-nextjs-template"
-              target="_blank"
-            >
-              from the GitHub repo
-            </a>
-            !
-          </span>
-        </li>
-        <li className="text-l">
-          👇
-          <span className="ml-2">
-            Try asking e.g. <code>What is it like to be a pirate?</code> below!
+            <strong> Budget and Timeline:</strong> Clarity Understand how long
+            your project will take and how much it will likely cost, with
+            options for every budget.
           </span>
         </li>
       </ul>
@@ -70,10 +45,10 @@ export default function Home() {
   );
   return (
     <ChatWindow
-      endpoint="api/chat"
-      emoji="🏴‍☠️"
-      titleText="Patchy the Chatty Pirate"
-      placeholder="I'm an LLM pretending to be a pirate! Ask me about the pirate life!"
+      endpoint="api/archr-assistant"
+      emoji=""
+      titleText="Archr Assistant"
+      placeholder="Please enter your message here"
       emptyStateComponent={InfoCard}
     ></ChatWindow>
   );
